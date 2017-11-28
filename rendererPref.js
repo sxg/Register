e05f54a156d73584c556763c5438a8a30942162e
Satyam Ghodasara <sghodas@gmail.com>
2017-11-28 16:13:28 -0500

    Preferences are now loaded from settings

--- rendererPref.js
+++ rendererPref.js
@@ -5,6 +5,9 @@ const settings = require('electron-settings')
 const path = require('path')
 
 //// UI components
+// Window
+const window = remote.getCurrentWindow()
+
 // Inputs
 const broccoliPathInput = document.getElementById('input-broccoli-path')
 const platformInput = document.getElementById('input-platform')
@@ -20,21 +23,49 @@ const labels = [broccoliPathLabel, platformLabel, deviceLabel]
 const broccoliPathButton = document.getElementById('button-broccoli-path')
 const saveButton = document.getElementById('button-save')
 
+//// Model
+let broccoliPath
+let rtvPath
+let platform
+let device
+
 //// UI actions
+// Load preferences
+window.webContents.on('did-finish-load', () => {
+    loadPreferences()
+})
+
 // Browse BROCCOLI path
 broccoliPathButton.addEventListener('click', event => {
-    dialog.showOpenDialog(remote.getCurrentWindow(), {properties: ['openDirectory']},
+    dialog.showOpenDialog(window, {properties: ['openDirectory']},
         filePaths => {
             if (filePaths && filePaths[0]) {
                 hideErrors()
-                broccoliPathInput.value = filePaths[0]
+                broccoliPath = filePaths[0]
+                broccoliPathInput.value = broccoliPath
                 osString = process.platform === 'darwin' ? 'Mac' : 'Linux'
-                rtvPath = path.join(filePaths[0], 'compiled', 'Bash', osString, 'Release', 'RegisterTwoVolumes')
-                settings.set('BROCCOLIPath', filePaths[0])
+                rtvPath = path.join(broccoliPath, 'compiled', 'Bash', osString, 'Release', 'RegisterTwoVolumes')
+                settings.set('BROCCOLIPath', broccoliPath)
+                settings.set('RTVPath', rtvPath)
             } else {
                 showError(broccoliPathLabel)
             }
         })
+    }
+)
+
+// Save settings
+saveButton.addEventListener('click', event => {
+    hideErrors()
+    if (!platformInput.value) {
+        showError(platformLabel)
+    } else if (!deviceInput.value) {
+        showError(deviceLabel)
+    } else {
+        settings.set('OpenCLPlatform', parseInt(platformInput.value))
+        settings.set('OpenCLDevice', parseInt(deviceInput.value))
+        window.close()
+    }
 })
 
 // Helpers
@@ -46,4 +77,21 @@ const hideErrors = function() {
 
 const showError = function(label) {
     label.style.display = 'inline-block'
+}
+
+const loadPreferences = function() {
+    broccoliPath = settings.get('BROCCOLIPath')
+    rtvPath = settings.get('RTVPath')
+    platform = settings.get('OpenCLPlatform')
+    device = settings.get('OpenCLDevice')
+
+    if (broccoliPath) {
+        broccoliPathInput.value = broccoliPath
+    }
+    if (platform) {
+        platformInput.value = platform.toString()
+    }
+    if (device) {
+        deviceInput.value = device.toString()
+    }
 }
