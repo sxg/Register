385d546d81a25277b3870758334db7d226cf5add
Satyam Ghodasara <sghodas@gmail.com>
2017-11-30 00:02:47 -0500

    main.js is now tabbed at 4 spaces

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const electron = require('electron')
 const {app, BrowserWindow, Menu} = electron
 
 // Node dependencies
+const settings = require('electron-settings')
 const path = require('path')
 const url = require('url')
 const fs = require('fs')
@@ -15,40 +16,44 @@ let tempPath
 let broccoliPath
 
 function createWindow () {
-  // Create the browser window.
-  mainWindow = new BrowserWindow({width: 1000, height: 375, resizable: false})
+    // Create the browser window.
+    mainWindow = new BrowserWindow({width: 1000, height: 375, resizable: false})
 
-  // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname, 'index.html'),
-    protocol: 'file:',
-    slashes: true
-  }))
+    // and load the index.html of the app.
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }))
 
-  Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate))
+    Menu.setApplicationMenu(Menu.buildFromTemplate(menuTemplate))
 
-  // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
-    fs.readdirSync(tempPath).forEach(function(file) {
-      filePath = path.join(tempPath, file)
-      fs.unlinkSync(filePath)
+    // Emitted when the window is closed.
+    mainWindow.on('closed', function () {
+        fs.readdirSync(tempPath).forEach(function(file) {
+            filePath = path.join(tempPath, file)
+            fs.unlinkSync(filePath)
+        })
+        fs.rmdirSync(tempPath)
+
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        mainWindow = null
     })
-    fs.rmdirSync(tempPath)
 
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    mainWindow = null
-  })
-
-  // Send the template and BROCCOLI paths to the renderer
-  mainWindow.webContents.on('did-finish-load', () => {
-    tempPath = path.join(app.getPath('temp'), 'Register')
-    if (!fs.existsSync(tempPath)) {
-      fs.mkdirSync(tempPath)
-    }
-    mainWindow.webContents.send('Message-TempPath', tempPath)
-  })
+    // Send the template and BROCCOLI paths to the renderer
+    mainWindow.webContents.on('did-finish-load', () => {
+        tempPath = path.join(app.getPath('temp'), 'Register')
+        if (!fs.existsSync(tempPath)) {
+            fs.mkdirSync(tempPath)
+        }
+        mainWindow.webContents.send('Message-TempPath', tempPath)
+        if (settings.has('BROCCOLIPath')) {
+            broccoliPath = settings.get('BROCCOLIPath')
+            mainWindow.webContents.send('Message-BROCCOLIPath', broccoliPath)
+        }
+    })
 }
 
 // This method will be called when Electron has finished
@@ -58,19 +63,19 @@ app.on('ready', createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', function () {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On OS X it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', function () {
-  // On OS X it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) {
-    createWindow()
-  }
+    // On OS X it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (mainWindow === null) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
